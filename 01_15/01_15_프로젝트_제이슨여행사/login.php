af8878c6031f888c251c307fb9ceaad8e2194515
ps9610 <ps961001@gamil.com>
2021-01-15 11:12:07 +0900

    지금부턴 서버이용해야함

---
+++ 01_15/01_15_프로젝트_제이슨여행사/login.php
@@ -0,0 +1,29 @@
+<?
+    /* include_once : 페이지를 한번만 불러온다. */
+    /* include_once ("./header.php") : header.php라는 파일을 불러온다. */
+    include_once("./header.php")
+?>
+
+<main id="main">
+    <section id="section1">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="section2">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?
+    include_once("./footer.php")
+?>
